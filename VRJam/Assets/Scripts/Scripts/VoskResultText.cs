718b636e30a2615723655679342040d27019f340
Mufasakong <Halipha.haidary@gmail.com>
2024-11-29 11:11:44 +0100

    Voice thingy in the game

--- VRJam/Assets/Scripts/Scripts/VoskResultText.cs
+++ VRJam/Assets/Scripts/Scripts/VoskResultText.cs
@@ -6,7 +6,7 @@ public class VoskResultText : MonoBehaviour
 {
     public VoskSpeechToText VoskSpeechToText;
     public TextMeshPro ResultText;
-
+    public bool CheckInput = false;
     void Awake()
     {
         VoskSpeechToText.OnTranscriptionResult += OnTranscriptionResult;
@@ -15,6 +15,7 @@ public class VoskResultText : MonoBehaviour
     private void OnTranscriptionResult(string obj)
     {
         //Debug.Log(obj);
+        if (CheckInput) {
         var result = new RecognitionResult(obj);
         ResultText.text = "";
         for (int i = 0; i < result.Phrases.Length; i++)
@@ -27,5 +28,6 @@ public class VoskResultText : MonoBehaviour
             ResultText.text += result.Phrases[i].Text;
         }
     	ResultText.text += "\n";
+        }
     }
 }
